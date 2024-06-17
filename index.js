297a6027f6f8493afcf4ebcb9820ecd1d75363b4
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2024-06-17 15:22:16 +0200

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+
+import unzipper from 'unzipper'
+
+fs.createReadStream('message export.zip.001')
+  .pipe(unzipper.Parse())
+  .on('entry', entry => {
+    const fileName = entry.path
+    const type = entry.type
+    const size = entry.vars.uncompressedSize
+
+    console.log(`File: ${fileName}, Type: ${type}, Size: ${size}`)
+
+    if (type === 'File') {
+      let content = ''
+
+      entry.on('data', chunk => { content += chunk.toString() })
+      entry.on('end', () => { console.log(`Content of ${fileName}:`, content) })
+    } else {
+      entry.autodrain()
+    }
+  })
+  .on('error', err => {
+    console.error('Error:', err)
+  })
+  .on('close', () => {
+    console.log('Extraction complete')
+  })
