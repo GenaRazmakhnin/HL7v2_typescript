41a95dfd4398a3f25a7821ee593a1ab1551085bb
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2024-06-19 18:51:44 +0200

    test

--- index.js
+++ index.js
@@ -71,42 +71,38 @@ const upload = async (content) => {
 
 const loadFile = async (file) => {
   data = []
+  // console.log(file)
 
-  return new Promise((res, rej) => {
-    try {
-      fs.createReadStream(file)
-      .pipe(unzipper.Parse())
-      .on('entry', async entry => {
-        const fileName = entry.path
-        const type = entry.type
-    
-        if (type === 'File') {
-          let content = ''
-          
-          entry.on('data', chunk => { content += chunk.toString() })
-          entry.on('end', async () => { data.push(content) })
-        } else {
-          entry.autodrain()
-        }
-      })
-      .on('error', err => {
-        console.error('Error:', err)
-      })
-      .on('close', async () => {
-        console.log('Extraction complete', data.length)
-    
-        for (let item of data) {
-          await upload(item);
-        }
-    
-        console.log("done")
-        res()
-      })
-
-    } catch(e) {
-      rej(e)
-    }
-  })      
+  return new Promise((res) => {
+    fs.createReadStream(file)
+    .pipe(unzipper.Parse())
+    .on('entry', async entry => {
+      const fileName = entry.path
+      const type = entry.type
+  
+      if (type === 'File') {
+        let content = ''
+        
+        entry.on('data', chunk => { content += chunk.toString() })
+        entry.on('end', async () => { data.push(content) })
+      } else {
+        entry.autodrain()
+      }
+    })
+    .on('error', err => {
+      console.error('Error:', err)
+    })
+    .on('close', async () => {
+      console.log('Extraction complete', data.length)
+  
+      for (let item of data) {
+        await upload(item);
+      }
+  
+      console.log("done")
+      res()
+    })
+  })  
 } 
 
 const getFiles = async () => {
@@ -127,13 +123,32 @@ const getFiles = async () => {
   })
 }
 
+async function isValidZip(filePath) {
+  return new Promise((resolve, reject) => {
+      fs.open(filePath, 'r', (err, fd) => {
+          if (err) {
+              return reject(err);
+          }
+          const buffer = Buffer.alloc(4);
+          fs.read(fd, buffer, 0, 4, 0, (err) => {
+              fs.close(fd, () => {
+                  if (err) { return reject(err) }
+                  const signature = buffer.toString('hex');
+                  resolve(signature === '504b0304');
+              });
+          });
+      });
+  });
+}
+
 const loadFiles = async () => {
   const result = await getFiles()
 
   for (let item of result) {
     console.log(item)
     try {
-      await loadFile(item);
+      // await loadFile(item);
+      await isValidZip(item);
     } catch(error) {
       console.log(error)
     }
