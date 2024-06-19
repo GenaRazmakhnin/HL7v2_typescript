05d33194026cab92e31276a3145f1c2eccb0a5a6
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2024-06-19 14:37:33 +0200

    test

--- index.js
+++ index.js
@@ -16,25 +16,27 @@ const getFormat = (gender) => {
 let errors = 0;
 
 const upload = async (content) => {
+  const { data } = await axios.post('http://localhost:8080/rpc', {
+    method: 'hl7v2.core/parse',
+    params: { message: content }
+  });
+  
+  const patient = data.result.parsed.patient_group.patient
+  
+  // console.dir(patient, { depth: 5 })
+  
+  const phone = (patient.telecom || []).find(i => i.system === 'phone')?.value || null
+  const city = patient.address?.[0]?.city || null
+  const pid = patient.identifier?.find(i => i.system === 'HL7.PID')?.value || null
+  const ssn = patient.identifier?.find(i => i.system === 'ssn')?.value || null
+  const first_name = patient.name?.[0]?.given?.[0] || null
+  const last_name =  patient.name?.[0]?.family || null
+  const birth_date = patient.birthDate || null
+  const gender = getFormat(patient.gender)
+
   try {
     // console.log(content)
-    const { data } = await axios.post('http://localhost:8080/rpc', {
-      method: 'hl7v2.core/parse',
-      params: { message: content }
-    });
     
-    const patient = data.result.parsed.patient_group.patient
-    
-    // console.dir(patient, { depth: 5 })
-    
-    const phone = (patient.telecom || []).find(i => i.system === 'phone')?.value || null
-    const city = patient.address?.[0]?.city || null
-    const pid = patient.identifier?.find(i => i.system === 'HL7.PID')?.value || null
-    const ssn = patient.identifier?.find(i => i.system === 'ssn')?.value || null
-    const first_name = patient.name?.[0]?.given?.[0] || null
-    const last_name =  patient.name?.[0]?.family || null
-    const birth_date = patient.birthDate || null
-    const gender = getFormat(patient.gender)
 
     await axios.post('http://jupyter-service.aidbox-dev.svc.cluster.local/patient', {
       pid, ssn, phone,
@@ -48,7 +50,15 @@ const upload = async (content) => {
 
     count++
   } catch (error) {
-    console.error(`Error for ${fileName}:`, error)
+    console.error({
+      pid, ssn, phone,
+      first_name, last_name,
+      birth_date, gender, city,
+      telecom_json: patient.telecom || [],
+      identifier_json: patient.identifier || [],
+      name_json: patient.name ||  [],
+      address_json: patient.address || []
+    })
     errors = errors + 1;
   }
 
@@ -80,6 +90,7 @@ fs.createReadStream('/home/aidbox/temp/message export.zip.001')
 
     for (let item of data) {
       await upload(item);
-
     }
+
+    console.log("done")
   })
