8e06914233978d9514fdce694e00ab9baee33657
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2024-07-08 12:10:27 +0200

    update script

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const loadFile = async (file) => {
     }
 
     return new Promise((resolve) => {
-      setTimeout(resolve(data), 1000)
+      setTimeout(() => resolve(data), 1000)
     })
   })
 
